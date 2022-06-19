7b26abe2ea913bd6e4e91db4f17ff3ee673a463b
MarceloDutra <marcellodutra010@yahoo.com.br>
2022-06-19 12:27:03 -0300

    update

--- Segunta-tab-imc/interacao.js
+++ Segunta-tab-imc/interacao.js
@@ -1,8 +1,8 @@
 //const calcular = document.getElementById('calcular')
 
 function imc(){
-    const peso = document.getElementById('peso02').value// converte o valor direto na variavel.
-    const altura = document.getElementById('altura02').value
+    const peso = document.getElementById('peso').value// converte o valor direto na variavel.
+    const altura = document.getElementById('altura').value
     const res = document.getElementById('res')
 
     if(peso !== '' && altura !== ''){ 
@@ -11,15 +11,15 @@ function imc(){
 
         let classificacao = ''
 
-        if(valorImc < 18.5){
+        if(valorImc < 18.5 ){
             classificacao = 'Esta abaixo do peso'
-        }else if(valorImc >= 18.5 && valorImc <= 24.9){
+        }else if(valorImc >= 18.5 && valorImc < 24.9){
             classificacao = 'Peso normal'
-        }else if(valorImc >= 25 && valorImc <= 29.9){
+        }else if(valorImc >= 24.9 && valorImc < 29.9){
             classificacao = 'Sobre peso'
-        }else if(valorImc >= 30 && valorImc <= 34.9){
+        }else if(valorImc >= 29.9 && valorImc < 34.9){
             classificacao = 'Obesidade grau 1'
-        }else if(valorImc >= 35 && valorImc <= 39.9){
+        }else if(valorImc >= 34.9 && valorImc < 39.9){
             classificacao = 'Obesidade grau 2'
         }else{
             classificacao = 'Obesidade grau 3'
