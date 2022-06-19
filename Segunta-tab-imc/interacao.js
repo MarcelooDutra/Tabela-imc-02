813cca910270d86aa9c44e7040f4fdab60fcf8a7
MarceloDutra <marcellodutra010@yahoo.com.br>
2022-06-18 23:33:30 -0300

    tebela imc 02

---
+++ Segunta-tab-imc/interacao.js
@@ -0,0 +1,36 @@
+//const calcular = document.getElementById('calcular')
+
+function imc(){
+    const peso = document.getElementById('peso02').value// converte o valor direto na variavel.
+    const altura = document.getElementById('altura02').value
+    const res = document.getElementById('res')
+
+    if(peso !== '' && altura !== ''){ 
+       
+        const valorImc = (peso / (altura * altura)).toFixed(1)
+
+        let classificacao = ''
+
+        if(valorImc < 18.5){
+            classificacao = 'Esta abaixo do peso'
+        }else if(valorImc >= 18.5 && valorImc <= 24.9){
+            classificacao = 'Peso normal'
+        }else if(valorImc >= 25 && valorImc <= 29.9){
+            classificacao = 'Sobre peso'
+        }else if(valorImc >= 30 && valorImc <= 34.9){
+            classificacao = 'Obesidade grau 1'
+        }else if(valorImc >= 35 && valorImc <= 39.9){
+            classificacao = 'Obesidade grau 2'
+        }else{
+            classificacao = 'Obesidade grau 3'
+
+            res.textContent = `O resultado do seu imc é: ${valorImc} e a classificação é: ${classificacao}`
+        }
+        
+    }else{
+        res.textContent = '[ERROR] Preencha os dados!'
+
+    }
+
+}
+//calcular.addEventListener('click', imc)
